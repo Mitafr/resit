4cdff4b58bd1d154acd51a145a8361236752ac30
MitaFR <martin.pelletier78@hotmail.com>
2025-07-13 19:10:01 +0200

    refactor: Simplify Pi71 parsing logic and improve readability

--- src/protocol/pi/pi71.rs
+++ src/protocol/pi/pi71.rs
@@ -29,34 +29,24 @@ impl Pi for Pi71 {
             auth_type: None,
             auth_procedure: None,
         };
-        if let Ok((data, auth)) =
-            nom::bits::complete::bool::<_, nom::error::Error<(&[u8], usize)>>((data, 1usize))
-        {
-            pi.auth = auth;
-            pi.auth_type =
-                nom::bits::complete::take::<_, u8, _, nom::error::Error<(&[u8], usize)>>(1usize)(
-                    data,
-                )
-                .map(|(_, algo)| match algo {
-                    0 => Some(AuthAlgo::Rsa),
-                    1 => Some(AuthAlgo::Des),
-                    _ => None,
-                })
-                .ok()
-                .flatten();
-            pi.auth_procedure =
-                nom::bits::complete::take::<_, u8, _, nom::error::Error<(&[u8], usize)>>(1usize)(
-                    data,
-                )
-                .map(|(_data, proc_type)| match proc_type {
-                    0 => Some(AuthProcedure::CertExchange),
-                    1 => Some(AuthProcedure::ThreeWayAuth),
-                    2 => Some(AuthProcedure::ThreeWayAuthDesOnly),
-                    _ => None,
-                })
-                .ok()
-                .flatten();
+        let (data, auth) = nom::bytes::complete::take(1usize)(data)?;
+        pi.auth = auth[0] == 1;
+        if !pi.auth {
+            return Ok((data, pi));
         }
+        let (data, auth_type) = nom::bytes::complete::take(1usize)(data)?;
+        pi.auth_type = match auth_type[0] {
+            0 => Some(AuthAlgo::Rsa),
+            1 => Some(AuthAlgo::Des),
+            _ => unreachable!(),
+        };
+        let (data, auth_procedure) = nom::bytes::complete::take(1usize)(data)?;
+        pi.auth_procedure = match auth_procedure[0] {
+            0 => Some(AuthProcedure::CertExchange),
+            1 => Some(AuthProcedure::ThreeWayAuth),
+            2 => Some(AuthProcedure::ThreeWayAuthDesOnly),
+            _ => unreachable!(),
+        };
         Ok((data, pi))
     }
 
@@ -68,5 +58,32 @@ impl Pi for Pi71 {
             buf.push(self.auth_procedure.unwrap() as u8);
         }
         buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_no_auth() {
+        let pi = Pi71::parse(&[0, 0, 0]).unwrap().1;
+        assert_eq!(pi.auth, false);
+        assert_eq!(pi.auth_type, None);
+        assert_eq!(pi.auth_procedure, None);
+        assert_eq!(pi.as_bytes(), [0]);
+    }
+
+    #[test]
+    fn test_parse_pi71() {
+        let pi = Pi71::parse(&[1, 0, 0]).unwrap().1;
+        assert_eq!(pi.auth, true);
+        assert_eq!(pi.auth_type, Some(AuthAlgo::Rsa));
+        assert_eq!(pi.auth_procedure, Some(AuthProcedure::CertExchange));
+        assert_eq!(pi.as_bytes(), [1, 0, 0]);
+        let pi = Pi71::parse(&[1, 1, 0]).unwrap().1;
+        assert_eq!(pi.auth, true);
+        assert_eq!(pi.auth_type, Some(AuthAlgo::Des));
+        assert_eq!(pi.auth_procedure, Some(AuthProcedure::CertExchange));
     }
 }
